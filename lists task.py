d9df636150e4fc316dc0ca34b5faab5737675485
James Guan <james.guan@abingdon.org.uk>
2023-09-15 14:36:23 +0100

    classwork 15/9

--- lists task.py
+++ lists task.py
@@ -3,9 +3,9 @@ name_list = ['Ed', 'William', 'Toby', 'Freddie', 'Rohan', 'Ian', 'Matthew', 'Gav
 for i in range(3):
     name = input("Type in a name: ")
     name_list.append(name)
-print(name_list)
+print(", ".join(name_list[:-1]) + ' and ' + name_list[-1])
 print("The third name is: " + name_list[2])
 last_seven = []
 for i in range(7,14):
     last_seven.append(name_list[i])
-print("The last seven names are: " + str(last_seven))
+print("The last seven names are " + str(last_seven))
