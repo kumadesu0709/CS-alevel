b3e4ede7d8edcc06d57392b80b742d68fdd61cf6
James Guan <james.guan@abingdon.org.uk>
2024-01-17 12:56:27 +0000

    16/1/24

--- CS alevel 2024lent term/homework/12.1.24.text files.py
+++ CS alevel 2024lent term/homework/12.1.24.text files.py
@@ -1,6 +1,9 @@
 import PyQt6.QtWidgets as qtw
 from PyQt6.QtWidgets import QMessageBox
 from PyQt6.QtWidgets import QFileDialog
+from PyQt6.QtWidgets import QToolBar
+import PyQt6.QtGui as qtg
+import sys
 
 class MainWindow(qtw.QMainWindow):
 
@@ -8,6 +11,33 @@ class MainWindow(qtw.QMainWindow):
         super().__init__()
         self.setWindowTitle("Text Editor")
 
+        save_action = qtg.QAction('Save', self)
+        save_action.setShortcut('Ctrl+S')
+        save_action.triggered.connect(self.save_clicked)
+
+        open_action = qtg.QAction('Open', self)
+        open_action.setShortcut('Ctrl+O')
+        open_action.triggered.connect(self.open_clicked)
+
+        quit_action = qtg.QAction('\0Quit', self)
+        quit_action.setShortcut('Ctrl+Q')
+        quit_action.triggered.connect(self.quit_clicked)
+
+
+        character_count = qtg.QAction('Character count', self)
+        character_count.setShortcut('Ctrl+C')
+        character_count.triggered.connect(self.count_character)
+
+
+        menubar = self.menuBar()
+        file_menu = menubar.addMenu("File")
+        file_menu.addAction(open_action)
+        file_menu.addAction(save_action)
+        file_menu.addAction(character_count)
+        file_menu.addSeparator()
+        file_menu.addAction(quit_action)
+
+        
         vlayout = qtw.QVBoxLayout()
         hlayout = qtw.QHBoxLayout()
 
@@ -36,16 +66,34 @@ class MainWindow(qtw.QMainWindow):
     
     def open_clicked(self):
         filename, _ = QFileDialog.getOpenFileName(self, "Open File", ".", "Text Files (*.txt *.html)")
-        print(filename)
+        if filename:
+            with open(filename, 'r') as text_file:
+                text = text_file.read()
+                self.main_text.setPlainText(text)
 
     def save_clicked(self):
         filename, _ = QFileDialog.getSaveFileName(self, "Save File", ".", "Text Files (*.txt *.html)")
-        print(filename)
+        text = self.main_text.toPlainText()
+        if filename:
+            with open(filename, 'w') as text_file:
+                text_file.write(text)
 
     def quit_clicked(self):
         button = QMessageBox.warning(self,"Hold up", "Are you sure you want to quit", QMessageBox.StandardButton.Ok| QMessageBox.StandardButton.Cancel)
-        if button == QMessageBox.OK:
+        if button == QMessageBox.StandardButton.Ok:
             self.close()
+    
+    def count_character(self):
+        text = self.main_text.toPlainText()
+        length = len(text)
+        QMessageBox.information(self, "CHaracter Length", f"The length of your text is: {length}", QMessageBox.StandardButton.Ok)
+
+
+
+
+
+
+
 
 app = qtw.QApplication([])
 
