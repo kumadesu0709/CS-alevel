c5b6240d59c86eb23779480a6afd30fb4153768e
James Guan <james.guan@abingdon.org.uk>
2024-04-30 14:16:38 +0100

    30/4/24 cw

--- CS alevel 2024 summer term/classwork/23,4,24, ascii image.py
+++ CS alevel 2024 summer term/classwork/23,4,24, ascii image.py
@@ -1,11 +1,11 @@
 from PIL import Image
 image = Image.open("IMG_3341.JPG")
 image = image.convert("HSV")
-image = image.resize((150,100))
+image = image.resize((170,100))
 pixels = image.load()
 showing_image = ""
 for y in range(100):
-    for x in range(150):
+    for x in range(170):
         v = pixels[x,y][2]
         if v in range(0,64):
             showing_image = showing_image + " "
@@ -15,6 +15,6 @@ for y in range(100):
             showing_image = showing_image + "="
         if v in range(192,256):
             showing_image = showing_image + "#"
-        if x == 149:
+        if x == 169:
             showing_image = showing_image + "\n"
 print(showing_image)
